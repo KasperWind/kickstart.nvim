e296d7f6770a8cc5ba3779d11dac79c94d074b61
Kasper Wind <kasperwind79@gmail.com>
2023-02-10 22:49:29 +0100

    adding svelte

--- init.lua
+++ init.lua
@@ -222,7 +222,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').git_files, { desc
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'dart', 'haskell', 'bash', 'dockerfile', 'cmake', 'yaml', 'toml' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'dart', 'haskell', 'bash', 'dockerfile', 'cmake', 'yaml', 'toml', 'svelte' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
