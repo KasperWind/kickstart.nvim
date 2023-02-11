fc3853e6bd33c6f01c037c48165ace92278da648
Kasper Wind <kasperwind79@gmail.com>
2023-02-11 10:49:19 +0100

    changed color scheme options

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,9 @@
 
 vim.o.termguicolors = true
+--vim.o.background = light
+--
+vim.g.gruvbox_material_better_performance = 1
 vim.cmd [[colorscheme gruvbox-material]]
---vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 require('lualine').setup {
   options = {
