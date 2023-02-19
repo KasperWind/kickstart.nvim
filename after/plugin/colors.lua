f09806b654a78d8c095cefff7542de947876a58f
Kasper Wind <kasperwind79@gmail.com>
2023-02-19 11:23:48 +0100

    adding time specific color background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,14 @@
 
 vim.o.termguicolors = true
 -- use command line :set background=light 
---vim.o.background = light
+
+local temp = os.date("*t", os.time())
+if temp.hour >= 8 and temp.hour <= 17 then
+  vim.o.background = light
+else
+  vim.o.background = dark  
+end
+
 vim.g.gruvbox_material_better_performance = 1
 vim.cmd [[colorscheme gruvbox-material]]
 
