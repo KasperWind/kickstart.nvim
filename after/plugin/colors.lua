752465b33c3adb08f024da6cb580ca16c79f7b53
Kasper Wind <kasperwind79@gmail.com>
2023-02-22 20:05:45 +0100

    adding time depend theme colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,16 +2,18 @@
 vim.o.termguicolors = true
 -- use command line :set background=light 
 
+
 local temp = os.date("*t", os.time())
-if temp.hour >= 8 and temp.hour <= 17 then
-  vim.o.background = light
+local h = tonumber(temp.hour)
+if h > 8 and h < 17 then
+  vim.o.background = "light"
 else
-  vim.o.background = dark  
+  vim.o.background = "dark"
 end
 
 vim.g.gruvbox_material_better_performance = 1
-vim.cmd [[colorscheme gruvbox-material]]
 
+vim.cmd [[colorscheme gruvbox-material]]
 require('lualine').setup {
   options = {
     icons_enabled = true,
